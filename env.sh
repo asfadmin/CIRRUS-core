46756a81ef7bf8a391fbd59bfc1bbf528a05abfd
Kevin Beam <kbeam@nsidc.org>
2020-03-11 22:11:56 +0000

    Export the selected profile as AWS_PROFILE.
    (cherry picked from commit 6cac995c7c414d41be6d9027b1d3434659465887)

--- env.sh
+++ env.sh
@@ -3,11 +3,10 @@
 if (( $# != 3 )); then
     echo "Usage: source env.sh aws_profile_name deploy_name maturity"
 else
-    profile_name=$1
-
-    export AWS_ACCESS_KEY_ID=`aws configure get aws_access_key_id --profile $profile_name`
-    export AWS_SECRET_ACCESS_KEY=`aws configure get aws_secret_access_key --profile $profile_name`
-    export AWS_REGION=`aws configure get region --profile $profile_name`
+    export AWS_PROFILE=$1
+    export AWS_ACCESS_KEY_ID=`aws configure get aws_access_key_id --profile $AWS_PROFILE`
+    export AWS_SECRET_ACCESS_KEY=`aws configure get aws_secret_access_key --profile $AWS_PROFILE`
+    export AWS_REGION=`aws configure get region --profile $AWS_PROFILE`
     export DEPLOY_NAME=$2
     export MATURITY=$3
 fi
