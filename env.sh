9441383b64c1aac45e6670ee1928e0d95524c4c1
McKade <mckades@gmail.com>
2020-04-22 09:26:19 -0800

    Fixed merge issues

--- env.sh
+++ env.sh
@@ -3,6 +3,8 @@
 AWS_ACCESS_KEY_ID=$(aws configure get aws_access_key_id --profile "$AWS_PROFILE")
 AWS_SECRET_ACCESS_KEY=$(aws configure get aws_secret_access_key --profile "$AWS_PROFILE")
 AWS_REGION=$(aws configure get region --profile "$AWS_PROFILE")
+AWS_ACCOUNT_ID=$(aws sts get-caller-identity --query "Account" --output text)
+AWS_ACCOUNT_ID_LAST4=${AWS_ACCOUNT_ID: -4:4}
 
 if (( $# != 3 )); then
     echo "Usage: source env.sh aws_profile_name deploy_name maturity"
@@ -11,6 +13,8 @@ else
     export AWS_ACCESS_KEY_ID
     export AWS_SECRET_ACCESS_KEY
     export AWS_REGION
+    export AWS_ACCOUNT_ID
+    export AWS_ACCOUNT_ID_LAST4
     export DEPLOY_NAME=$2
     export MATURITY=$3
 fi
