3b9f7170edeba438c08e3ef1cedd237c2fa7c373
Kevin Beam <kbeam@nsidc.org>
2020-05-14 08:09:12 -0600

    Fix env.sh script so it uses and exports the correct profile, etc

--- env.sh
+++ env.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
-AWS_ACCESS_KEY_ID=$(aws configure get aws_access_key_id --profile "$AWS_PROFILE")
-AWS_SECRET_ACCESS_KEY=$(aws configure get aws_secret_access_key --profile "$AWS_PROFILE")
-AWS_REGION=$(aws configure get region --profile "$AWS_PROFILE")
-AWS_ACCOUNT_ID=$(aws sts get-caller-identity --query "Account" --output text)
-AWS_ACCOUNT_ID_LAST4=${AWS_ACCOUNT_ID: -4:4}
-
 if (( $# != 3 )); then
     echo "Usage: source env.sh aws_profile_name deploy_name maturity"
 else
     export AWS_PROFILE=$1
+
+    AWS_ACCESS_KEY_ID=$(aws configure get aws_access_key_id --profile "$AWS_PROFILE")
+    AWS_SECRET_ACCESS_KEY=$(aws configure get aws_secret_access_key --profile "$AWS_PROFILE")
+    AWS_REGION=$(aws configure get region --profile "$AWS_PROFILE")
+    AWS_ACCOUNT_ID=$(aws sts get-caller-identity --query "Account" --output text)
+    AWS_ACCOUNT_ID_LAST4=${AWS_ACCOUNT_ID: -4:4}
+
     export AWS_ACCESS_KEY_ID
     export AWS_SECRET_ACCESS_KEY
     export AWS_REGION
@@ -17,4 +18,12 @@ else
     export AWS_ACCOUNT_ID_LAST4
     export DEPLOY_NAME=$2
     export MATURITY=$3
+
+    echo "CIRRUS environment:"
+    echo "  AWS_PROFILE:          $AWS_PROFILE"
+    echo "  AWS_REGION:           $AWS_REGION"
+    echo "  AWS_ACCOUNT_ID:       $AWS_ACCOUNT_ID"
+    echo "  AWS_ACCOUNT_ID_LAST4: $AWS_ACCOUNT_ID_LAST4"
+    echo "  DEPLOY_NAME:          $DEPLOY_NAME"
+    echo "  MATURITY;             $MATURITY"
 fi
