102328d705c925ba89e75dbf5ea69829d35b8b24
Lindsley, Chris <lindsleycj@ornl.gov>
2020-06-09 10:47:30 -0400

    add more comments to delete-AccessTokensTable.sh

--- scripts/cumulus-v1.23.2/delete-AccessTokensTable.sh
+++ scripts/cumulus-v1.23.2/delete-AccessTokensTable.sh
@@ -3,6 +3,9 @@
 # script to remove AccessTokensTable per the migration instructions
 #  https://github.com/nasa/cumulus/releases/tag/v1.23.2
 #
+# need to run this first to set up environment:
+#   $ source env.sh <profile-name> <deploy-name> <maturity>
 #
+
 aws dynamodb delete-table \
     --table-name $DEPLOY_NAME-cumulus-$MATURITY-AccessTokensTable
