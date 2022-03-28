bf02210cdcf06898eb592f75cb8c1427a39fbe35
Lindsley, Chris <lindsleycj@ornl.gov>
2022-03-28 16:45:31 -0400

    update migration script to use payload, upgrade TEA to 1.0.2

--- scripts/cumulus-v10.1.1/data_migration1.sh
+++ scripts/cumulus-v10.1.1/data_migration1.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-# run this lambda after deploying the data_migration1 and data-persistence
-# modules, and before deploying the cumulus module
+# run this lambda after deploying the data-persistence module and before deploying the cumulus module
 
-aws lambda invoke --function-name $DEPLOY_NAME-cumulus-$MATURITY-data-migration1 $DEPLOY_NAME-cumulus-$MATURITY-dm1.log
+PAYLOAD=$(echo '{"forceRulesMigration": true}')
+
+aws lambda invoke --function-name $DEPLOY_NAME-cumulus-$MATURITY-data-migration1 \
+  --payload "$PAYLOAD" $DEPLOY_NAME-cumulus-$MATURITY-dm1.log
