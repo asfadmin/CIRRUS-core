48ba6b6c2a917ad53be10c4c11e9e452b242fcb7
mckadesorensen <dmsorensen@alaska.edu>
2023-11-21 09:27:36 -0900

    feat: upload cirrus versions to s3

--- tf/locals.tf
+++ tf/locals.tf
@@ -3,6 +3,9 @@ locals {
   aws_account_id       = data.aws_caller_identity.current.account_id
   aws_account_id_last4 = substr(data.aws_caller_identity.current.account_id, -4, 4)
 
+  cirrus_core_version  = var.CIRRUS_CORE_BRANCH == "HEAD" ? var.CIRRUS_CORE_TAG: var.CIRRUS_CORE_BRANCH
+  cirrus_daac_version  = var.CIRRUS_DAAC_BRANCH == "HEAD" ? var.CIRRUS_DAAC_TAG: var.CIRRUS_DAAC_BRANCH
+
   default_tags = {
     Deployment = local.prefix
   }
--- tf/variables.tf
+++ tf/variables.tf
@@ -6,3 +6,19 @@ variable "MATURITY" {
   type    = string
   default = "dev"
 }
+
+variable "CIRRUS_CORE_BRANCH" {
+  type    = string
+}
+
+variable "CIRRUS_DAAC_BRANCH" {
+  type    = string
+}
+
+variable "CIRRUS_CORE_TAG" {
+  type    = string
+}
+
+variable "CIRRUS_DAAC_TAG" {
+  type    = string
+}
