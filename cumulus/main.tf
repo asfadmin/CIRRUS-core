cfa411a41aa72596f3d012b7eff54b9317e789c1
Lindsley, Chris <lindsleycj@ornl.gov>
2025-05-09 16:42:24 -0400

    upgrade Cumulus to v20.1.2

--- cumulus/main.tf
+++ cumulus/main.tf
@@ -1,5 +1,5 @@
 module "cumulus" {
-  source = "https://github.com/nasa/cumulus/releases/download/v20.1.1/terraform-aws-cumulus.zip//tf-modules/cumulus"
+  source = "https://github.com/nasa/cumulus/releases/download/v20.1.2/terraform-aws-cumulus.zip//tf-modules/cumulus"
 
   cumulus_message_adapter_lambda_layer_version_arn = data.terraform_remote_state.daac.outputs.cma_layer_arn
 
