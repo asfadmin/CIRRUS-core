fc582faab66a92dd4c9e5af55e2d0b37826f6d44
Jonathan Kovarik <Jkovarik@users.noreply.github.com>
2026-07-02 09:46:03 -0600

    Update to Cumulus v22.3.0 (#265)
    * Update to Cumulus v22.3.0
    
    * Add data-persistence partition config values for 22.3.x

--- data-persistence/variables.tf
+++ data-persistence/variables.tf
@@ -17,9 +17,11 @@ variable "CIRRUS_DAAC_VERSION" {
 
 variable "db_partition_config" {
   type = object({
-    executions_total_years = number
-    granules_count         = number
-    files_count            = number
+    executions_total_years       = number
+    granules_count               = number
+    files_count                  = number
+    granules_global_unique_count = number
+    files_global_unique_count    = number
   })
 
   description = <<EOT
@@ -27,11 +29,14 @@ variable "db_partition_config" {
     - executions_total_years: How many years worth of quarterly partitions to generate for 'executions'.
     - granules_count: The number of hash/bigint-based partitions to create for the 'granules' table.
     - files_count: The number of hash/bigint-based partitions to create for the 'files' table.
+    - granules_global_unique_count: The number of hash/bigint-based partitions to create for the 'granules_global_unique' table.
+    - files_global_unique_count: The number of hash/bigint-based partitions to create for the 'files_global_unique' table.
   EOT
-
   default = {
-    executions_total_years = 2
-    granules_count         = 512
-    files_count            = 1024
+    executions_total_years       = 2
+    granules_count               = 512
+    files_count                  = 1024
+    granules_global_unique_count = 16
+    files_global_unique_count    = 64
   }
 }
